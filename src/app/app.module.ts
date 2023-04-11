48d2ba29ca1c456c646d884ab9adf5641699235f
Tyler Bath <tylerbath@gmail.com>
2023-04-11 23:46:44 +0000

    Delete now unused items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Route, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { WordAssociateComponent } from './lits1/word-associate.component'; 
 import { MessagePageComponent } from './message-page/message-page.component';
 import { TaskMessageComponent } from './task-message/task-message.component';
 import { TaskMessageTestingComponent } from './task-message/task-message-testing.component';
-
 import { WordDirective } from './word.directive';
 import { WordService } from './word.service'; 
-import { Lits3Component } from './lits3/lits3.component';
-import { Lits5Component } from './lits5/lits5.component';
 import { MainPageComponent } from './main-page/main-page.component';
-import { WordThreeDisplayComponent } from './lits3/word-three-display.component';
-import { WordFiveDisplayComponent } from './lits5/word-five-display.component';
-import { WordAssociateThreeComponent } from './lits3/word-associate-three.component';
-import { WordAssociateFiveComponent } from './lits5/word-associate-five.component';
-import { MessageThreeComponent } from './message-three/message-three.component';
-import { MessageFiveComponent } from './message-five/message-five.component';
-import { WordListThreeComponent } from './lits3/word-list-three.component';
-import { WordListFiveComponent } from './lits5/word-list-five.component';
-import { TaskMessageThreeComponent } from './task-message-three/task-message-three.component';
-import { TaskMessageFiveComponent } from './task-message-five/task-message-five.component';
-import { WordInputDisplayComponent } from './word-input-display/word-input-display.component';
 import { WordAssociateInputComponent } from './word-input-display/word-associate-input.component';
-import { WordInputDisplayThreeComponent } from './word-input-display-three/word-input-display-three.component';
-import { WordAssociateInputThreeComponent } from './word-input-display-three/word-associate-input-three.component';
-import { WordInputDisplayFiveComponent } from './word-input-display-five/word-input-display-five.component';
-import { WordAssociateInputFiveComponent } from './word-input-display-five/word-associate-input-five.component';
 import { CommonModule } from '@angular/common';
 import { DisableOnEnterDirective } from './disable-on-enter.directive';
 import { FailTestComponent } from './fail-test.component'; 
 import { PassTestComponent } from './pass-test.component';
-import { WordListInputThreeComponent } from './word-input-display-three/word-list-input-three.component'; 
-import { WordListInputFiveComponent } from './word-input-display-five/word-list-input-five.component'; 
 
 //yoannes , a static property needs to be created to be used as global variable
 import { Injectable } from '@angular/core'; 
@@ -62,34 +39,13 @@ import { Injectable } from '@angular/core';
     WordAssociateComponent,
     MessagePageComponent,
     WordDirective,
-    Lits3Component,
-    Lits5Component,
     MainPageComponent,
-    WordThreeDisplayComponent,
-    WordFiveDisplayComponent,
-    WordAssociateThreeComponent,
-    WordAssociateFiveComponent,
-    MessageThreeComponent,
-    MessageFiveComponent,
-    WordListThreeComponent,
-    WordListFiveComponent,
     TaskMessageComponent,
     TaskMessageTestingComponent,
-    TaskMessageThreeComponent,
-    TaskMessageFiveComponent,
-    WordInputDisplayComponent,
     WordAssociateInputComponent,
-    WordInputDisplayThreeComponent,
-    WordAssociateInputThreeComponent,
-    WordInputDisplayFiveComponent,
-    WordAssociateInputFiveComponent,
     DisableOnEnterDirective,
     FailTestComponent,
-    PassTestComponent,
-    WordListInputThreeComponent,
-    WordListInputFiveComponent,
-
-  
+    PassTestComponent
   ],
   bootstrap: [ AppComponent ]
 })
